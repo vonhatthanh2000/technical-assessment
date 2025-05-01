bd90200833525537dfdb9daa8f079019f75792ed
thanhvodev <nthanhatee@gmail.com>
2025-05-01 16:43:05 +0700

    update: add polygon amoy testnet because mumbai is deprecated

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -22,46 +22,60 @@ const config: HardhatUserConfig = {
   networks: {
     mainnet: {
       url: "https://rpc.ankr.com/eth",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     mumbai: {
       url: "https://rpc.ankr.com/polygon_mumbai",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     sepolia: {
       url: "https://eth-sepolia.public.blastapi.io",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     polygon: {
       url: "https://polygon-rpc.com",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     binance: {
       url: "https://bsc.rpc.blxrbdn.com",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     bsct: {
       url: "https://endpoints.omniatech.io/v1/bsc/testnet/public",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     goerli: {
       url: "https://goerli.blockpi.network/v1/rpc/public",
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
-    }
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    amoy: {
+      url: "https://polygon-amoy.g.alchemy.com/v2/AmrqP0mmcIj1N6wWbLkfqBogtpUEMblK",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   mocha: {
-      timeout: 1000000000000,
+    timeout: 1000000000000,
   },
   etherscan: {
     apiKey: {
-      mainnet: process.env.ETHERSCAN_API_KEY || '',
-      polygon: process.env.ETHERSCAN_API_KEY || '',
-      polygonMumbai: process.env.ETHERSCAN_API_KEY || '',
-      bsc: process.env.ETHERSCAN_API_KEY || '',
-      bscTestnet: process.env.ETHERSCAN_API_KEY || '',
-      sepolia: process.env.ETHERSCAN_API_KEY || ''
-    }
-  }
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+      polygon: process.env.ETHERSCAN_API_KEY || "",
+      polygonMumbai: process.env.ETHERSCAN_API_KEY || "",
+      bsc: process.env.ETHERSCAN_API_KEY || "",
+      bscTestnet: process.env.ETHERSCAN_API_KEY || "",
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      goerly: process.env.ETHERSCAN_API_KEY || "",
+      amoy: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
 };
 
 export default config;
